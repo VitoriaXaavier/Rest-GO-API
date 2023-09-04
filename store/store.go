9f1e6061fc7459bb1f91229bd85bc8455dac0044
Vitoria Xavier <vitoria_xavierd@outlook.com>
2023-09-04 16:01:54 -0300

    feat: adicionando o store da api

--- store/store.go
+++ store/store.go
@@ -1,1 +1,34 @@
 package store
+
+import (
+	"context"
+	"math/rand"
+	"fmt"
+	"time"
+	"github.com/VitoriaXaavier/Rest-GO-API/objects"
+)
+
+// Interface do banco para armazenar eventos
+type IEventStore interface {
+	Get (ctx context.Context, in *objects.GetRequest) (*objects.Event, error)
+	List (ctx context.Context, in *objects.ListRequest) ([]*objects.Event, error)
+	Create (ctx context.Context, in *objects.CreateRequest) error
+	UpDateDetails (ctx context.Context, in *objects.UpDateDetailsRequest) error
+	Cancel 	(ctx context.Context, in *objects.CancelRequest) error
+	Remarca (ctx context.Context, in *objects.RemarcaRequest) error
+	Delete (ctx context.Context, in *objects.DeletRequest) error
+}
+
+func init() {
+	rand.Seed(time.Now().UTC().Unix())
+}
+
+// Irá retornar um id exclusivel classificado com base no tempo
+func GenerateUniqueId() string {
+	word := []byte("0987654321")
+	rand.Shuffle(len(word), func(i,j  int) {
+		word[i], word[j] = word[j], word[i]
+	})
+	now := time.Now().UTC()
+	return fmt.Sprintf("%010v-%010v-%s", now.Unix(), now.Nanosecond(), string(word))
+}
