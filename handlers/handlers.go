51764d8aba580676299a778f46f08f8559c0287d
Vitoria Xavier <vitoria_xavierd@outlook.com>
2023-09-11 14:46:08 -0300

    feat: adicionando as implementaçoes das rotas

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -2,8 +2,12 @@
 package handlers
 
 import (
-	 "net/http"
-	 "github.com/VitoriaXaavier/Rest-GO-API/store"
+	"io/ioutil"
+	"net/http"
+
+	"github.com/VitoriaXaavier/Rest-GO-API/errors"
+	"github.com/VitoriaXaavier/Rest-GO-API/objects"
+	"github.com/VitoriaXaavier/Rest-GO-API/store"
 )
 type IEventHandler interface {
 	Get (w http.ResponseWriter, r *http.Request)
@@ -15,39 +19,158 @@ type IEventHandler interface {
 	Delete (w http.ResponseWriter, r *http.Request)
 }
 
+
 //usado para implementar a interface
 type handler struct {
-
+	store store.IEventStore
 }
 
 func NewEventHandler(store store.IEventStore) IEventHandler {
-	return &handler{}
+	return &handler{store: store}
 }
 
 func (h *handler) Get(w http.ResponseWriter, r *http.Request) {
-	panic("Implemente-me")
+	id := r.URL.Query().Get("id")
+	 if id == ""{
+		WriterError(w, errors.ErrValidEventIDIsRequired)
+		return
+	 }
+	evt, err := h.store.Get(r.Context(), &objects.GetRequest{ID: id})
+	if err != nil {
+		WriterError(w, err)
+		return
+	}
+	WriterResponse(w, &objects.EventResponseWrapper{Event: evt})
 }
 
 func (h *handler) List(w http.ResponseWriter, r *http.Request) {
-	panic("Implemente-me")
+	values := r.URL.Query()
+
+	after := values.Get("after")
+	nome := values.Get("nome")
+	limit, err := IntFromString(w, values.Get("limit"))
+	if err != nil {
+		return
+	}
+
+	// lista eventos
+	list, err := h.store.List(r.Context(), &objects.ListRequest{
+		Limt: limit,
+		After: after,
+		Nome: nome,
+	})
+	if err != nil {
+		WriterError(w, err)
+		return
+	}
+	WriterResponse(w, &objects.EventResponseWrapper{Events: list})
 }
 
 func (h *handler) Create(w http.ResponseWriter, r *http.Request) {
-	panic("Implemente-me")
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		WriterError(w, errors.ErrUnprocessableEntity)
+		return
+	}
+	evt := &objects.Event{}
+	if Unmarshal(w, data, evt) != nil {
+		return
+	}
+	if err != ChekSlot(evt.Slot) {
+		WriterError(w, err)
+		return
+	}
+	if err = h.store.Create(r.Context(), &objects.CreateRequest{Event: evt}); err != nil {
+		WriterError(w, err)
+		return
+	}
+	WriterResponse(w, &objects.EventResponseWrapper{Event: evt})
+
 }
 
 func (h *handler) UpDateDetails(w http.ResponseWriter, r *http.Request) {
-	panic("Implemente-me")
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		WriterError(w, errors.ErrUnprocessableEntity)
+		return
+	}
+	req := &objects.UpDateDetailsRequest{}
+	if Unmarshal(w, data, req) != nil {
+		return
+	}
+
+	// checa se evento existe
+	if _, err := h.store.Get(r.Context(), &objects.GetRequest{ID: req.ID}); err != nil {
+		WriterError(w, err)
+		return
+	}
+
+	if err = h.store.UpDateDetails(r.Context(), req); err != nil {
+		WriterError(w, err)
+		return
+	}
+	WriterResponse(w, &objects.EventResponseWrapper{})
+
 }
 
 func (h *handler) Cancel(w http.ResponseWriter, r *http.Request) {
-	panic("Implemente-me")
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		WriterError(w, errors.ErrValidEventIDIsRequired)
+		return
+	}
+
+	if _, err := h.store.Get(r.Context(), &objects.GetRequest{ID: id}); err != nil {
+		WriterError(w, err)
+		return
+	}
+
+	if err := h.store.Cancel(r.Context(), &objects.CancelRequest{ID: id}); err != nil {
+		WriterError(w, err)
+		return
+	}
+
+	WriterResponse(w, &objects.EventResponseWrapper{})
 }
 
 func (h *handler) Reschedule(w http.ResponseWriter, r *http.Request) {
-	panic("Implemente-me")
+	data, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		WriterError(w, errors.ErrUnprocessableEntity)
+		return
+	}
+
+	req := &objects.RemarcaRequest{}
+	if Unmarshal(w, data, req) != nil {
+		return
+	}
+
+	if err := ChekSlot(req.NewSlot); err != nil {
+		WriterError(w, err)
+		return
+	}
+
+	if _, err := h.store.Get(r.Context(), &objects.GetRequest{ID: req.ID}); err != nil {
+		WriterError(w, err)
+		return
+	}
+	if err = h.store.Remarca(r.Context(), req); err != nil {
+		WriterError(w, err)
+		return
+	}
+
+	WriterResponse(w, &objects.EventResponseWrapper{})
 }
 
 func (h *handler) Delete(w http.ResponseWriter, r *http.Request) {
-	panic("Implemente-me")
+	id := r.URL.Query().Get("id")
+	if id == "" {
+		WriterError(w, errors.ErrValidEventIDIsRequired)
+		return
+	}
+	if _, err := h.store.Get(r.Context(), &objects.GetRequest{ID: id}); err != nil {
+		WriterError(w, err)
+		return
+	}
+	WriterResponse(w,&objects.EventResponseWrapper{})
 }	
