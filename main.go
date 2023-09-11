35b8cd764783734d3a8e2a3083a3eb30b9a8e07e
Vitoria Xavier <vitoria_xavierd@outlook.com>
2023-09-11 14:44:27 -0300

    feat: atualizando o arquivo main

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"log"
+	"os"
+)
+
+func main() {
+	args := Args{
+		conn: "postgres://postgres:@localhost:5432/postgres?sslmode=disable",
+		port: ":8000", 
+	}
+
+	if conn := os.Getenv("DB_CONN"); conn != "" {
+		args.conn = conn
+	}
+	if port := os.Getenv("PORT"); port != "" {
+		args.port = ":" + port
+	}
+
+	if err := Run(args); err != nil {
+		log.Panicln(err)
+	}
+}
